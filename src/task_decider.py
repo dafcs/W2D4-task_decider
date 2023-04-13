0fe1986bddd9f407ba3ed923fd9e6bfbdd1c2d13
Daniel FCS <danielf.cs@outlook.com>
2023-04-13 13:53:31 +0100

    first commit

---
+++ src/task_decider.py
@@ -0,0 +1,39 @@
+# do make a class for tasks
+# do not make a class for get_prefered_option
+
+# wash dishes > cook dinner
+# cook dinner > clean windows
+# clean windows > wash dishes
+
+
+# def get_preferred_option(task1,task2):
+#     options = [task1,task2] 
+#     if "Wash Dishes" and "Cook Dinner" in options:
+#         return "Wash Dishes"
+#     elif "Cook Dinner" and "Clean Windows" in options:
+#         return "Cook Dinner"
+#     elif "Clean Windows" and "Wash Dishes" in options:
+#         return "Clean Windows"
+#     else:
+#         return "be serious fam"
+
+
+def get_preferred_option(task1,task2):
+    #key will always take preference over value
+    preferences = {
+        "Wash Dishes":["Cook Dinner"],
+        "Cook Dinner":["Clean Windows", "Do Ironing"],
+        "Clean Windows":["Wash Dishes", "Do Ironing"],
+        "Do Ironing" : ["Wash Clothes, Wash Dishes"],
+        "Wash Clothes": ["Cook Dinner", "Clean Windows"]
+        }
+    
+    for key in preferences:
+        if task1 == key and task2 in preferences[key]:
+            return(key)
+        if task2 == key and task1 in preferences[key]:
+            return(key)
+    return "be serious fam"
+
+
+get_preferred_option("Wash Dishes","Wash Dishes")
---
+++ tests/task_decider_test.py
@@ -0,0 +1,25 @@
+import unittest
+from src.task_decider import *
+from src.task import Task
+
+class TestDecider(unittest.TestCase):
+
+    
+
+    def test_task_decider_1(self):
+        self.result = get_preferred_option("Wash Dishes","Cook Dinner")
+        self.assertEqual("Wash Dishes", self.result)
+    def test_task_decider_2(self):
+        self.result = get_preferred_option("Wash Clothes","Cook Dinner")
+        self.assertEqual("Wash Clothes", self.result)
+    def test_task_decider_3(self):
+        self.result = get_preferred_option("Wash Dishes","Clean Windows")
+        self.assertEqual("Clean Windows", self.result)
+    def test_task_decider_4(self):
+        self.result = get_preferred_option("Clean Windows","Do Ironing")
+        self.assertEqual("Clean Windows", self.result)
+    def test_task_decider_5(self):
+        self.result = get_preferred_option("Do Ironing","Cook Dinner")
+        self.assertEqual("Cook Dinner", self.result)
+    def test_task_decider_fail(self):
+        self.assertEqual("be serious fam", get_preferred_option("Wash Dishes","Wash Dishes"))
